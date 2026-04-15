589d1666c88bf6b296206bcd717ce2eb3169863b
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:50:14 +0200

    Add removeDuplicates method to handle duplicates
    Implement a solution to remove duplicates from a sorted array, allowing at most two occurrences of each element.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Remove Duplicates from sorted array 2.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Remove Duplicates from sorted array 2.py
@@ -1,1 +1,27 @@
-print()
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        a = False
+        if len(nums) < 3:
+            return len(nums)  
+        if nums[0] != nums[1] :
+            j += 1
+        if nums[len(nums)-2] == nums[len(nums)-1] :  
+            a = True
+        for i in range(1,len(nums)-1):
+            if nums[i-1] == nums[i] and nums[i] != nums[i+1]:
+                nums[j] = nums[i]
+                nums[j+1] = nums[i]
+                j += 2
+            if  nums[i-1] != nums[i] and nums[i] != nums[i+1]: 
+                nums[j] = nums[i]
+                j += 1 
+        if nums[len(nums)-2] != nums[len(nums)-1] :
+            nums[j] = nums[len(nums)-1]
+            j += 1
+        if a == True :   
+            nums[j] = nums[len(nums)-1]
+            nums[j+1] = nums[len(nums)-1]
+            j += 2
+        print(nums)    
+        return j 
