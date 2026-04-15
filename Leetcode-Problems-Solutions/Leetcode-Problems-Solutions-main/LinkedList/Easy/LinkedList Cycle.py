9996f391f39734ddb2b7ea57294c34abdc66b93e
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:09:58 +0200

    Implement cycle detection in linked list

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/LinkedList/Easy/LinkedList Cycle.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/LinkedList/Easy/LinkedList Cycle.py
@@ -1,1 +1,18 @@
-print()
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head == None or head.next == None  :
+            return False
+        first = head
+        second = head
+        while first and first.next :
+            first = first.next.next
+            second = second.next
+            if first == second :
+                return True
+        return False
