bc6678c7b3582b20a12d0d97f81ed968e766a808
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:20:57 +0200

    Add BSTIterator class for in-order traversal
    Implemented a binary search tree iterator that performs in-order traversal.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Binary Search Tree Iterator.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Binary Search Tree Iterator.py
@@ -1,1 +1,34 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.lst = self.inorder(root)
+
+    def inorder(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def traverse(root):
+            if not root:
+                return
+            traverse(root.left)
+            res.append(root.val)
+            traverse(root.right)
+        traverse(root)
+        return res
+
+    def next(self) -> int:
+        return self.lst.pop(0)
+
+    def hasNext(self) -> bool:
+        return ( len(self.lst) > 0 )
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
