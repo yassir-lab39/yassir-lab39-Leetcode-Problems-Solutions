5ba2a76d6d840afebb11bb79ad9726bb1e9bf6a9
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:07:59 +0200

    Add method to calculate minimum arrows for balloons
    Implement the findMinArrowShots method to calculate the minimum number of arrows needed to burst balloons based on their intervals.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Intervals/Medium/Minimum Number of Arraows to Burst Ballons.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Intervals/Medium/Minimum Number of Arraows to Burst Ballons.py
@@ -1,1 +1,16 @@
-print()
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        nb = 0
+        sorted_points = sorted(points, key = lambda x : x[0])
+        print(sorted_points)
+        [i,j] = sorted_points[0]
+        for k in range(len(points)) :
+            i = max(i,sorted_points[k][0])
+            j = min(j,sorted_points[k][1])
+            if i > j :
+                nb += 1
+                i = sorted_points[k][0]
+                j = sorted_points[k][1]
+        return nb+1
+
+            
