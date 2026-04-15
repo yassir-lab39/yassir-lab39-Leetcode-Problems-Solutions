5431f7b352c444d9bf2de58fd79935e2e9fe5515
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:22:23 +0200

    Implement average of levels in binary tree

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeBFS/Easy/Average of Levels In Binary Tree.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeBFS/Easy/Average of Levels In Binary Tree.py
@@ -1,1 +1,26 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        if not root:
+            return []
+        l = [root]
+        res = [root.val]
+        while l != [] :
+            s = 0
+            n = len(l)
+            for i in range(n):
+                a = l.pop(0)
+                if a.left:
+                    l.append(a.left)
+                if a.right:
+                    l.append(a.right)
+            if l != []:
+                for i in range(len(l)):
+                    s += l[i].val
+                res.append(s/len(l))
+        return res
