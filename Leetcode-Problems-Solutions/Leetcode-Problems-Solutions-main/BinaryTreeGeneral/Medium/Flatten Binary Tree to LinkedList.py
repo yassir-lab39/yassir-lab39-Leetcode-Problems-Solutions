23ee3afe2041c6cabcebdc45bf39ceedea382c62
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:19:36 +0200

    Add flatten function for binary tree to linked list
    Implemented a solution to flatten a binary tree into a linked list using preorder traversal.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Flatten Binary Tree to LinkedList.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Flatten Binary Tree to LinkedList.py
@@ -1,1 +1,29 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorder(self,res: List[int], root: Optional[TreeNode]):
+        if not root:
+            return
+        res.append(root.val)
+        self.preorder(res,root.left)
+        self.preorder(res,root.right)
+        return res
+
+
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if not root :
+            return
+        res = self.preorder([],root)
+        if len(res) > 1:
+            current = root
+            current.left = None
+            for i in range(1,len(res)):
+                current.right = TreeNode(res[i])
+                current = current.right
+
+
+        
