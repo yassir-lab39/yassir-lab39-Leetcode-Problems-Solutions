57852206ac5cebbad2b7e19c22f56fb3c63f72c3
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:03:50 +0200

    Add twoSum function to Solution class
    Implement twoSum function to find indices of two numbers that add up to target.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Two Sum.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Two Sum.py
@@ -1,1 +1,12 @@
-print()
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        D = dict()
+        for i in range(len(nums)) :
+            if nums[i] in D :
+                if 2*nums[i] == target :
+                    return [D[nums[i]],i]
+            else :
+                D[nums[i]] = i
+            if target-nums[i] in D and D[target-nums[i]] != i :
+                return [i,D[target-nums[i]]]
+        return False
