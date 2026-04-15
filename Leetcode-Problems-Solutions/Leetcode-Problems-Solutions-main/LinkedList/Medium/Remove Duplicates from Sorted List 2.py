aed8e80dcb83a869a691204449e204afcf6655e0
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:13:04 +0200

    Implement deleteDuplicates method for linked list

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/LinkedList/Medium/Remove Duplicates from Sorted List 2.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/LinkedList/Medium/Remove Duplicates from Sorted List 2.py
@@ -1,1 +1,19 @@
-print()
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        L = []
+        v = 0.5
+        a = False
+        while current:
+            if a and v == current.val :
+                L.pop()
+                a = False
+            if v != current.val:
+                v = current.val
+                L.append(ListNode(v))  
+                a = True
+            current = current.next
+        while len(L) > 1:
+            a = L.pop()
+            L[-1].next = a
+        return L[0] if L else None
