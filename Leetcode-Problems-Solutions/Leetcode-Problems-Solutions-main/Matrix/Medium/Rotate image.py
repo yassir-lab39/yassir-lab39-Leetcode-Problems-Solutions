d97b8bcaea4ea73a25f7307988ef6bc9cd957728
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:00:45 +0200

    Add matrix rotation method to Solution class
    Implement rotation of a matrix in place by 90 degrees clockwise.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Matrix/Medium/Rotate image.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Matrix/Medium/Rotate image.py
@@ -1,1 +1,21 @@
-print()
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        i = 0
+        while i < n//2 :
+            copy = []
+            for k in range(i,n-i) :
+                copy.append(matrix[i][k])
+            for k in range(i,n-i) :
+                matrix[i][k] = matrix[n-k-1][i]
+            for k in range(i,n-i) :
+                matrix[k][i] = matrix[n-1-i][k]
+            for k in range(i,n-i) :
+                matrix[n-1-i][k] = matrix[n-k-1][n-1-i]
+            for k in range(i,n-i) :
+                matrix[k][n-1-i] = copy[k-i]
+            i += 1
+        
+
+        
+        
