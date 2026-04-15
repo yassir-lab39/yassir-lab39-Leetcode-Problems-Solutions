3a3feae76fe65b301c52c25660e7eda743cf630b
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:59:15 +0200

    Implement lengthOfLongestSubstring method

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Sliding Window/Medium/Longest Sbstring Without Repeating Characters.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Sliding Window/Medium/Longest Sbstring Without Repeating Characters.py
@@ -1,1 +1,19 @@
-print()
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "" :
+            return 0
+        j = 0
+        nb = 1
+        left, right = 0, 1
+        while left < len(s) and right < len(s) :
+            for i in range(left,right) :
+                if s[i] != s[right] :
+                    j += 1
+                else :
+                    j = 0
+            if nb < j+1 :
+                nb = j+1
+            right += 1
+            left = right-j-1
+            j = 0
+        return nb
