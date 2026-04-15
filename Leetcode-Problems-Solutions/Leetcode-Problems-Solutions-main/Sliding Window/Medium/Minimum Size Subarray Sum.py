b7ca754214ffb69411045e5806453b21181e6678
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:58:55 +0200

    Add minSubArrayLen method to Solution class
    Implement minSubArrayLen method to find minimum size subarray sum.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Sliding Window/Medium/Minimum Size Subarray Sum.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Sliding Window/Medium/Minimum Size Subarray Sum.py
@@ -1,1 +1,15 @@
-print()
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        s = nums[0]
+        i, j = 0, 1
+        nb = 0
+        while i < len(nums) :
+            while s < target and j < len(nums) :
+                s += nums[j]
+                j += 1
+            if s >= target :
+                if nb == 0 or nb > j-i :
+                    nb = j-i
+            s -= nums[i]
+            i += 1
+        return nb
