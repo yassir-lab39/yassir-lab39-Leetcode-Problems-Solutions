963281ef24e0d54ba05d6e244d2d59780312f364
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:52:43 +0200

    Implement productExceptSelf method in Solution class

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Produc of Array Except Self.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Produc of Array Except Self.py
@@ -1,1 +1,21 @@
-print()
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prod = 1
+        nb = 0
+        for i in range(n) :
+            if nums[i] != 0:
+                prod = prod*nums[i]
+            else :
+                j = i
+                nb += 1
+        if nb > 1 :
+            return [0]*n  
+        if nb == 1 :
+            return [0]*j + [int(prod)] + [0]*(n-j-1)
+        answer = []
+        for i in range(n) :
+            answer.append(int(prod/nums[i]))         
+        return answer     
+
+        
