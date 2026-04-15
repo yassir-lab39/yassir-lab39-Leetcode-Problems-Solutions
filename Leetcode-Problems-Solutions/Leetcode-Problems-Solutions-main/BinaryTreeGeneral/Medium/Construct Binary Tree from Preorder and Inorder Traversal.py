9a40f7fe4ca23f632d211ee453ae3e4164646226
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:18:14 +0200

    Add function to build binary tree from traversals
    Implemented the function to construct a binary tree from preorder and inorder traversal lists.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,1 +1,18 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if preorder == [] or inorder == []:
+            return None
+        res = TreeNode(preorder[0]) 
+        index = inorder.index(preorder[0])
+        left = inorder[:index]
+        right = inorder[index+1:]
+        res.left = self.buildTree(preorder[1:],left)
+        res.right = self.buildTree(preorder[1+index:],right) 
+        return res
