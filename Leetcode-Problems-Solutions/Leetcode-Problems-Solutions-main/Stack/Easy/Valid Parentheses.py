bbe49dea17552cb94699c0750b417c93fb2d106e
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:08:30 +0200

    Add valid parentheses validation logic
    Implement a solution to check for valid parentheses using a stack.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Stack/Easy/Valid Parentheses.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Stack/Easy/Valid Parentheses.py
@@ -1,1 +1,21 @@
-print()
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if s[0] in {'}',')',']'} or len(s)/2 != len(s)//2 :
+            return False
+        stack = [s[0]]
+        for c in s[1:] :
+            if c == ')' :
+                if stack == [] or stack[-1] != '(' :
+                    return False
+                stack.pop()
+            elif c == ']' :
+                if stack == [] or stack[-1] != '[' :
+                    return False
+                stack.pop()
+            elif c == '}' :
+                if stack == [] or stack[-1] != '{' :
+                    return False
+                stack.pop()
+            else :
+                stack.append(c)
+        return (stack==[])
