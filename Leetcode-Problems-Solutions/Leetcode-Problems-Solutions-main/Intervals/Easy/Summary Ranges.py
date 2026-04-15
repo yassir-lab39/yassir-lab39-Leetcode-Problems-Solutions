ec48fc6f08ee42572cc97161b6b12471d0908984
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:06:43 +0200

    Add summaryRanges function to summarize integer ranges
    Implement summaryRanges function to summarize ranges in a list of integers.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Intervals/Easy/Summary Ranges.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Intervals/Easy/Summary Ranges.py
@@ -1,1 +1,22 @@
-print()
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if nums == [] :
+            return []
+        a = nums[0]
+        b = 0.5
+        res = []
+        for i in range(1,len(nums)) :
+            if nums[i] == nums[i-1]+1 :
+                b = nums[i]
+            else :
+                if b == 0.5 :
+                    res.append(str(a))
+                else :
+                    res.append(str(a)+"->"+str(b))
+                    b = 0.5
+                a = nums[i]
+        if b == 0.5 :
+            res.append(str(a))
+        else :
+            res.append(str(a)+"->"+str(b))
+        return res
