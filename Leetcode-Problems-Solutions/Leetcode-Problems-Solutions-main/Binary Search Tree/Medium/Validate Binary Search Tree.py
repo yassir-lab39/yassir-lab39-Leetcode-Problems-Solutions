2ae9126f299836d1be35e2b01480fe0bd5fcb639
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:25:02 +0200

    Add isValidBST method to validate binary search tree
    Implement validation for binary search tree using in-order traversal.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Binary Search Tree/Medium/Validate Binary Search Tree.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Binary Search Tree/Medium/Validate Binary Search Tree.py
@@ -1,1 +1,21 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedValues(self, root: Optional[TreeNode]):
+        if not root:
+            return []
+        return self.sortedValues(root.left) + [root.val] + self.sortedValues(root.right)
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        sortedValues = self.sortedValues(root)
+        v = sortedValues[0]
+        for i in range(1,len(sortedValues)):
+            if v < sortedValues[i]:
+                v = sortedValues[i]
+            else:
+                return False
+        return True
