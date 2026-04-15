a88722fe4da597c63cf7871bd24065c0ee961f5e
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:24:39 +0200

    Add solution for Kth Smallest Element in a BST
    Implement kth smallest element in a BST using in-order traversal.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Binary Search Tree/Medium/Kth Smallest Element in a BST.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Binary Search Tree/Medium/Kth Smallest Element in a BST.py
@@ -1,1 +1,15 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedValues(self, root: Optional[TreeNode]):
+        if not root:
+            return []
+        return self.sortedValues(root.left) + [root.val] + self.sortedValues(root.right)
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        sortedValues = self.sortedValues(root)
+        return sortedValues[k-1]
