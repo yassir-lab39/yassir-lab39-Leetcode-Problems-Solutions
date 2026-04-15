4595a1873f5ba5e092edd9edd0d3fb769378d482
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:07:09 +0200

    Add merge function for merging intervals
    Implement merge function to merge overlapping intervals.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Intervals/Medium/Merge Intervals.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Intervals/Medium/Merge Intervals.py
@@ -1,1 +1,13 @@
-print()
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_intervals = sorted(intervals, key = lambda x : x[0])
+        res = [sorted_intervals[0]]
+        for i in range(len(sorted_intervals)) :
+            if sorted_intervals[i][0] <= res[-1][1] :
+                if sorted_intervals[i][1] > res[-1][1] :
+                    res[-1] = [res[-1][0],sorted_intervals[i][1]]
+            else :
+                res.append(sorted_intervals[i])
+        return res
+
+        
