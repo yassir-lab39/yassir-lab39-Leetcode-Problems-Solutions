43ff68f7e7d0b39f3ce39b3ce8fc0a4de4b6bbad
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:03:12 +0200

    Implement isAnagram method for checking anagrams

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Valid Anagram.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Valid Anagram.py
@@ -1,1 +1,17 @@
-print()
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        D = dict()
+        if len(s) != len(t) :
+            return False
+        for c in s :
+            if c not in D :
+                D[c] = 1
+            else :
+                D[c] += 1
+        for c in t :
+            if c in D :
+                D[c] -= 1
+        for c in s :
+            if D[c] != 0 :
+                return False
+        return True
