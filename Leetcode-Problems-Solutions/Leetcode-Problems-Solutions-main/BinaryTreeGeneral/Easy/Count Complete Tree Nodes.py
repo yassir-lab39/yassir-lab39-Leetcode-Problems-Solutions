3a27efaab13a003990f9bd35992b5b140c147117
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:17:45 +0200

    Add countNodes function for complete binary tree
    Implement function to count nodes in a complete binary tree.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Easy/Count Complete Tree Nodes.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Easy/Count Complete Tree Nodes.py
@@ -1,1 +1,27 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = deque([root])
+        level = 0
+        count = 2**level
+        while q != []:
+            for i in range(len(q)):
+                a = q.popleft()
+                if a.left:
+                    q.append(a.left)
+                else:
+                    return count + i*2
+                if a.right:
+                    q.append(a.right)
+                else:
+                    return count + i*2 + 1
+            level += 1
+            count += 2**level
+        return count
