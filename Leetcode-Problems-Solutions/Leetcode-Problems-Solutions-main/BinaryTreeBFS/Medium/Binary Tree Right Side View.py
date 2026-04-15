b86487e2000887a56f9ca6cd5f62a165f67335da
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:22:50 +0200

    Add right side view function for binary tree
    Implemented BFS to get right side view of binary tree.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeBFS/Medium/Binary Tree Right Side View.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeBFS/Medium/Binary Tree Right Side View.py
@@ -1,1 +1,23 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        l = [root]
+        res = [root.val]
+        while l != [] :
+            n = len(l)
+            for i in range(n):
+                a = l.pop(0)
+                if a.left:
+                    l.append(a.left)
+                if a.right:
+                    l.append(a.right)
+            if l != []:
+                res.append(l[-1].val)
+        return res
