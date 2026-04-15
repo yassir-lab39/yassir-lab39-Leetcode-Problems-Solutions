90447d2c289b00df8f5188d4906890327bf85c16
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:59:50 +0200

    Add Sudoku validation logic in Valid Sudoku.py
    Implement isValidSudoku method to validate a Sudoku board.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Matrix/Medium/Valid Sudoku.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Matrix/Medium/Valid Sudoku.py
@@ -1,1 +1,34 @@
-print()
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        L = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for i in range(9) :
+            for j in range(9) :
+                if board[i][j] != "." :
+                    if board[i][j] in L :
+                        L.remove(board[i][j])
+                    else :
+                        return False
+            L = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for i in range(9) :
+            for j in range(9) :
+                if board[j][i] != "." :
+                    if board[j][i] in L :
+                        L.remove(board[j][i])
+                    else :
+                        return False
+            L = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        i, j = 0, 0
+        while i < 9 :
+            while j < 9 :
+                for k in range(i,i+3) :
+                    for l in range(j, j+3) :
+                        if board[k][l] != "." :
+                            if board[k][l] in L :
+                                L.remove(board[k][l])
+                            else :
+                                return False
+                L = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                j += 3
+            j = 0
+            i += 3
+        return True
