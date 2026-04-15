4628b368e605981560e346284231587c663d4e06
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:50:38 +0200

    Add rotate function to rotate an array
    Implement rotate function to rotate array in place.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/RotateArray.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/RotateArray.py
@@ -1,1 +1,11 @@
-print()
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = k % len(nums)
+        L=[]
+        for i in range(0,n):
+            L.append(nums[len(nums)-n+i])
+        for i in range(0,len(nums)-n):
+            L.append(nums[i])   
+        nums[0:len(nums)-n] = L[0:len(nums)-n]
+        nums[len(nums)-n:len(nums)] = L[len(nums)-n:len(nums)]
+        
