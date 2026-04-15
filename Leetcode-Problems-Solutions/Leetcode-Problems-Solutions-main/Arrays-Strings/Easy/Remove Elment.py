f18458ee94fc5835474ff54c1caee45191244607
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:44:26 +0200

    Add removeElement method to remove specified values
    Implemented removeElement method to remove specified value from the list.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Remove Elment.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Remove Elment.py
@@ -1,1 +1,10 @@
-print()
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        L = []
+        for i in range(0,len(nums)):
+            if nums[i] != val:
+                L = L + [nums[i]]
+        nums[0:len(L)] = L 
+        nums[len(L):len(nums)] = (len(nums)-len(L))*["_"]
+        return len(L)
+                
