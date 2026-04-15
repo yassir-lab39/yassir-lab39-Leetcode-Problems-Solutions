b46cde5854c19c442aa31a955a5ae9e85911cddf
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:44:53 +0200

    Add removeDuplicates method to Solution class
    Implemented a method to remove duplicates from a sorted array in place.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Remove Duplicates from sorted array.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Remove Duplicates from sorted array.py
@@ -1,1 +1,26 @@
-print()
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 1 
+        nb = 0
+        i = 0 
+        while i < len(nums) -1: 
+            if nums[i] == nums[i+1]:
+                nums[i] = 101 
+            else:
+                nums[nb],nums[i] = nums[i], nums[nb]
+                nb += 1 
+            i += 1 
+        if nb == 0:
+            nums[0] = nums[-1]
+            return 1 
+        if nums[i] != nums[nb-1]:
+            nums[nb] = nums[i]
+            nb += 1 
+        return nb
+    
+            
+                
+            
+        
+        
