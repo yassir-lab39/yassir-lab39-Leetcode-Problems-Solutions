8092507a878a50424949ce6ae4176fbe4b346268
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:52:13 +0200

    Add jump method to Solution class
    Implement jump method to solve Jump Game II problem.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Jump Game 2.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Jump Game 2.py
@@ -1,1 +1,20 @@
-print()
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        i = 0
+        n = len(nums)
+        nb = 1
+        if n == 1 :
+            return 0
+        while i < n :
+            if i + nums[i] >= n-1 :
+                return nb
+            maxi = 0
+            maxi_id = 0
+            for j in range(i+1,i+1+nums[i]) :
+                if maxi < nums[j] + j - i :
+                    maxi = nums[j] + j - i
+                    maxi_id = j
+            i = maxi_id
+            nb += 1
+        return nb            
+
