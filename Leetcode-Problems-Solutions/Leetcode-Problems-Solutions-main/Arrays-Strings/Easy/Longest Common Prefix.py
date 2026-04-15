4eefeff14c1064b4477aebfa60852b616e1241ee
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:49:05 +0200

    Implement longestCommonPrefix method in Solution class

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Longest Common Prefix.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Longest Common Prefix.py
@@ -1,1 +1,15 @@
-print()
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        s = strs[0]
+        if s == "" :
+            return s
+        for i in range(1, len(strs)) :
+            if strs[i] == "" :
+                return ""
+            if s[0] != strs[i][0] :
+                return ""
+            for j in range(0,len(s)) :
+                if j >= len(strs[i]) or s[j] != strs[i][j]  :
+                    s = s[:j]
+                    break
+        return s
