ead50041a394406c0144b87123fbf0a900b6b8ad
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:16:32 +0200

    Add solution for checking symmetric binary tree
    Implemented the isSymmetric method to check if a binary tree is symmetric.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Easy/Symmetric Tree.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Easy/Symmetric Tree.py
@@ -1,1 +1,19 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def fct(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and self.fct(left.left, right.right) and self.fct(left.right, right.left)
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root :
+            return True
+        return self.fct(root.left,root.right)
+        
