3ed351b88ce43427a19afc6439a030e7c5d83e2c
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:21:29 +0200

    Add Lowest Common Ancestor solution for binary tree
    Implement the Lowest Common Ancestor algorithm for a binary tree using parent pointers.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Lowest Common Ancestor of a Binary Tree.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Lowest Common Ancestor of a Binary Tree.py
@@ -1,1 +1,30 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def parent(self, root : 'TreeNode', D : dict):
+        if root :
+            if root.left:
+                D[root.left] = root
+                self.parent(root.left, D)
+            if root.right:
+                D[root.right] = root
+                self.parent(root.right, D)
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        parents = dict()
+        self.parent(root, parents)
+
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parents.get(p)
+        while q:
+            if q in ancestors:
+                return q
+            q = parents.get(q)
+
