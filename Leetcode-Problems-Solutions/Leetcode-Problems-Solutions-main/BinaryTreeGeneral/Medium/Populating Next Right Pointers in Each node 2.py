043ff3ff9cc0443dee690267e650dbe9170c09ee
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:19:17 +0200

    Add solution for populating next right pointers
    Implemented a solution to connect next right pointers in a binary tree.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Populating Next Right Pointers in Each node 2.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Medium/Populating Next Right Pointers in Each node 2.py
@@ -1,1 +1,30 @@
-print()
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        l = [root]
+        while l != []:
+            n = len(l)
+            for i in range(n):
+                a = l.pop(0)
+                if i != n-1:
+                    a.next = l[0]
+                if a.left:
+                    l.append(a.left)
+                if a.right:
+                    l.append(a.right)
+        return root
+            
+
+
+        
