35f98fed78b3287cf4f6068a418e15bf5eb66b22
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:45:27 +0200

    Add majorityElement function to find majority element
    Implemented a solution to find the majority element in a list of integers using a dictionary to count occurrences.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Majority Element.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Majority Element.py
@@ -1,1 +1,14 @@
-print()
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        D = {}
+        for i in range(len(nums)) :
+            if nums[i] in D.keys() :
+                D[nums[i]] += 1
+            else :
+                D[nums[i]] = 1
+            if D[nums[i]] > len(nums)//2 :
+                return nums[i]    
+        
+                
+
+        
