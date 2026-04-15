c98e3c356661813fbe5ea5b9127a5b51d7abb5d9
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:49:37 +0200

    Implement strStr function to find first occurrence

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Find the Index of the First occurrence in a String.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Find the Index of the First occurrence in a String.py
@@ -1,1 +1,14 @@
-print()
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        nb = 0
+        i = 0
+        while i < len(haystack):
+            if haystack[i] == needle[nb] :
+                nb += 1
+                i += 1
+            else :
+                i -= (nb-1)
+                nb = 0
+            if nb == len(needle) :
+                return i-nb
+        return -1
