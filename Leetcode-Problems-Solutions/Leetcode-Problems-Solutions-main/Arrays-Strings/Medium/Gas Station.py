23ccb36324907e245a4df737046c12278796fd8a
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:53:14 +0200

    Implement canCompleteCircuit method in Gas Station

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Gas Station.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Gas Station.py
@@ -1,1 +1,17 @@
-print()
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        L = [a - b for a, b in zip(gas, cost)]
+        if sum(L) < 0 :
+            return -1
+        i = 0
+        while i < len(L) :
+            index = (i+1) % len(L)
+            fuel = L[i]
+            while index != i and fuel > 0 :
+                fuel += L[index]  
+                index = (index+1) % len(L) 
+            if index == i and fuel >= 0 :
+                return index 
+            if fuel <= 0 :
+                i = index  
+        return -1           
