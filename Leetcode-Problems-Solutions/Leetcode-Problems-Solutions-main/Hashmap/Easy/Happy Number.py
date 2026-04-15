9de893d00ef34539e6d11a613ae4e84bff8f1ccc
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:04:22 +0200

    Add isHappy function to check for happy numbers
    Implement isHappy function to determine if a number is happy.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Happy Number.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Happy Number.py
@@ -1,1 +1,15 @@
-print()
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s = 0
+        D = set()
+        while s != 1 :
+            s = 0
+            while n > 0 :
+                s += (n % 10)**2
+                n = n // 10
+            if s in D :
+                return False
+            D.add(s)
+            n = s
+        return True
+            
