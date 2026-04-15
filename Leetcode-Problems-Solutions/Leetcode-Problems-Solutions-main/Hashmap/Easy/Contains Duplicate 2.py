bc2a544fb24cb5aa794de65241e12ad8168a2f5a
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:04:49 +0200

    Implement containsNearbyDuplicate function

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Contains Duplicate 2.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Contains Duplicate 2.py
@@ -1,1 +1,11 @@
-print()
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        D = dict()
+        for i in range(len(nums)) :
+            if nums[i] not in D :
+                D[nums[i]] = i
+            else :
+                if i-D[nums[i]] <= k :
+                    return True
+                D[nums[i]] = i
+        return False
