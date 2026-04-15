d3676e42f5a02aa2030d68cf102abdd80103b187
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:17:00 +0200

    Add path sum functionality to binary tree solution
    Implement path sum calculation for binary tree.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Easy/Path Sum.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/BinaryTreeGeneral/Easy/Path Sum.py
@@ -1,1 +1,27 @@
-print()
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def path(self, root: Optional[TreeNode],s: set):
+        if not root:
+            return
+        if root.left:
+            root.left.val += root.val
+            self.path(root.left,s)
+        if root.right:
+            root.right.val += root.val
+            self.path(root.right,s)
+        if not root.left and not root.right:
+            s.add(root.val)
+
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        s = set()
+        self.path(root,s)
+        print(s)
+        return (targetSum in s) 
+        
