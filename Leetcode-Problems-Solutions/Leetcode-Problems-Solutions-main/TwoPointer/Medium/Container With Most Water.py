c7a0e984685598e1709794844ae3f12abb48b358
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:57:42 +0200

    Add maxArea function for Container With Most Water
    Implemented the maxArea function to calculate the maximum water container area using the two-pointer technique.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/TwoPointer/Medium/Container With Most Water.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/TwoPointer/Medium/Container With Most Water.py
@@ -1,1 +1,12 @@
-print()
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height)-1
+        area = min(height[left], height[right])*right
+        while left <= right :
+            if height[left] < height[right] :
+                left += 1   
+            else :
+                right -= 1
+            if area < min(height[left], height[right])*(right-left) :
+                area = min(height[left], height[right])*(right-left)
+        return area
