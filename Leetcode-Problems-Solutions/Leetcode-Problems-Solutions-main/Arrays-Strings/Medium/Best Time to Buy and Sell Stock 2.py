84acba0867b24fe4ca9bbedf5a4a24c7cfb9cf2e
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:51:14 +0200

    Add maxProfit method to Solution class
    Implement maxProfit method to calculate maximum profit from stock prices.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Best Time to Buy and Sell Stock 2.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Best Time to Buy and Sell Stock 2.py
@@ -1,1 +1,13 @@
-print()
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        nb = 0
+        for j in range(0,len(prices)-1):
+            if prices[j] < prices[j+1]:
+                nb += 1
+                if j+1 == len(prices)-1:
+                    profit = profit - prices[j+1-nb] + prices[j+1]
+            else :
+                profit = profit - prices[j-nb] + prices[j]
+                nb = 0
+        return profit
