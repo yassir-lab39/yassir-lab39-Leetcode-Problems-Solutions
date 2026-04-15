9302d49682e1e2076a0b06cba5fdab21560f918d
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:02:32 +0200

    Add isIsomorphic function to check string isomorphism
    Implement isIsomorphic method to check if two strings are isomorphic.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Isomorphic Strings.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Isomorphic Strings.py
@@ -1,1 +1,15 @@
-print()
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        D = dict()
+        for i in range(len(s)) :
+            if s[i] in D :
+                if D[s[i]] != t[i] :
+                    return False
+            else :
+                D[s[i]] = t[i]
+        S = []
+        for c in D :
+            if D[c] in S :
+                return False
+            S.append(D[c])
+        return True
