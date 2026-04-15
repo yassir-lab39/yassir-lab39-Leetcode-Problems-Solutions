6f644f71586a5092e843a6eba22f236b36556e52
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:48:43 +0200

    Add lengthOfLastWord method to Solution class
    Implement lengthOfLastWord method to calculate the length of the last word in a string.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Length of Last Word.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Length of Last Word.py
@@ -1,1 +1,12 @@
-print()
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        i = len(s)-1
+        while i >= 0 :
+            if s[i] != ' ' :
+                length += 1
+            if s[i] == ' ' and length != 0 :
+                i = 0
+            i -= 1
+        return length
+
