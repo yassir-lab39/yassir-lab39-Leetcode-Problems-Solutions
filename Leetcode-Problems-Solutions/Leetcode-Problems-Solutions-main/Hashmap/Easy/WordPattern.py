e84b80e2f3ff5e3fc17c991a540321bde036029b
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:02:51 +0200

    Implement word pattern matching in WordPattern.py

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/WordPattern.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/WordPattern.py
@@ -1,1 +1,19 @@
-print()
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        D = dict()
+        j = 0
+        word = ""
+        for i in range(len(pattern)) :
+            while j < len(s) and s[j] != ' ' :
+                word += s[j]
+                j += 1
+            if pattern[i] in D :
+                if D[pattern[i]] != word :
+                    return False
+            else :
+                if word in D.values() :
+                    return False
+                D[pattern[i]] = word
+            j += 1
+            word = ""
+        return (j == len(s)+1)
