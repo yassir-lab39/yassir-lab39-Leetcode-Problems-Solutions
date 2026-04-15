89075f0a76f94bbed216c17b86a076544b724464
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:01:07 +0200

    Add setZeroes function to modify matrix in place
    Implement solution to set matrix zeroes based on zero elements.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Matrix/Medium/Set Matrix Zeroes.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Matrix/Medium/Set Matrix Zeroes.py
@@ -1,1 +1,15 @@
-print()
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows = []
+        columns = []
+        for i in range(len(matrix)) :
+            for j in range(len(matrix[0])) :
+                if matrix[i][j] == 0 :
+                    rows.append(i)
+                    columns.append(j)
+        for j in range(len(matrix[0])) :
+            for i in range(len(rows)) :
+                matrix[rows[i]][j] = 0   
+        for i in range(len(matrix)) :
+            for j in range(len(columns)) :
+                matrix[i][columns[j]] = 0     
