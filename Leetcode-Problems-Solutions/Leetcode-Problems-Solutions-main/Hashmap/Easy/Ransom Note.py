3760b2aa735d366e91fb467ec9520d8966b3b68a
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:02:08 +0200

    Implement canConstruct method for Ransom Note problem

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Ransom Note.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Hashmap/Easy/Ransom Note.py
@@ -1,1 +1,15 @@
-print()
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        D = dict()
+        for c in magazine :
+            if c in D :
+                D[c] += 1
+            else :
+                D[c] = 1
+        for c in ransomNote :
+            if c not in D :
+                return False
+            D[c] -= 1    
+            if D[c] < 0 :
+                return False
+        return True
