fd2fcda16d5d4c5eee5d1d0df7ea6a34ec6dd21f
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:53:46 +0200

    Add solution to reverse words in a string
    Implemented a class Solution with a method to reverse the words in a string.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Revers Words in a String.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Revers Words in a String.py
@@ -1,1 +1,18 @@
-print()
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s1 = ""
+        s2 = ""
+        a = 0
+        for i in range(len(s)-1,-1,-1) :
+            if s[i] != " " :
+                s1 = s[i] + s1
+                a = 1
+            if s[i] == " " and a == 1 :
+                s2 = s2 + s1 + " "
+                s1 = ""
+                a = 0
+            if i == 0 and s[0] != " " :
+                s2 = s2 + s1
+        if s2[-1] == " " :
+            return s2[:len(s2)-1]
+        return s2
