bb67374a43e7785f2f5bc381a53c4211f5e52b5d
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 13:08:56 +0200

    Add simplifyPath function for path simplification
    Implement a solution to simplify Unix file paths using a stack.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Stack/Medium/Simplify Path.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Stack/Medium/Simplify Path.py
@@ -1,1 +1,45 @@
-print()
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        a = True
+        nb = 0
+        for c in path :
+            if c == '/' :
+                if nb == 2 and len(stack) > 1 :
+                    stack.pop()
+                    while stack[-1] != '/' :
+                        stack.pop()
+                elif nb > 2 :
+                    stack.append('.'*nb)
+                    stack.append('/')
+                nb = 0
+                if a :
+                    stack.append(c)
+                    a = False
+            elif c == '.' :
+                if stack[-1] not in {'/'} :
+                    stack.append('.')
+                else : 
+                    nb += 1
+            else :
+                if nb != 0 :
+                    stack.append('.'*nb)
+                    nb = 0
+                stack.append(c)
+                a = True
+
+        if nb == 2 and len(stack) > 1 :
+            stack.pop()
+            while stack[-1] != '/' :
+                stack.pop()
+            stack.pop()
+        elif nb > 2 :
+            stack.append('.'*nb)
+        if stack == [] :
+            return "/"
+        if len(stack) > 1 and ( path[-1] == '/' or stack[-1] == '/' ) :
+            return "".join(stack[:len(stack)-1])
+        return "".join(stack)
+
+
+        
