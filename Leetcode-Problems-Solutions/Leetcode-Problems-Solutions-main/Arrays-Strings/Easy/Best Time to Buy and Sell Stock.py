8f5f58bd216695a30fce52a15c3e81c6fef78e74
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:45:56 +0200

    Implement maxProfit function to calculate profit

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Best Time to Buy and Sell Stock.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Easy/Best Time to Buy and Sell Stock.py
@@ -1,1 +1,10 @@
-print()
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxi = 0
+        mini = prices[0]
+        n = len(prices)
+        for i in range(n):
+            mini = min(mini, prices[i])
+            maxi = max(maxi, prices[i]-mini)
+        return maxi    
+        
