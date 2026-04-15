77e0c6fe6628551f3a3c2f5c575c6b02a33b570d
yassircchakir <cchakiryassir@gmail.com>
2026-04-15 12:51:42 +0200

    Add canJump method to Solution class
    Implement canJump method to determine if the end of the array can be reached.

--- Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Jump Game.py
+++ Leetcode-Problems-Solutions/Leetcode-Problems-Solutions-main/Arrays-Strings/Medium/Jump Game.py
@@ -1,1 +1,20 @@
-print()
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n == 1:
+            return True
+        i = 0
+        p = nums[0]
+        while i < n :
+            if p == 0 :
+                return False
+            p -= 1
+            i += 1
+            if i == n-1 :
+                return True
+            if p < nums[i] :
+                p = nums[i]        
+                        
+
+
+        
